d7eacbac92e11a9d664780f6118ea230ba821ae0
Muhammad Fahim <fahimmia616@gmail.com>
2021-07-20 09:19:06 +0600

    logical operators lessons complete

---
+++ comparison and logical Operators.js
@@ -0,0 +1,27 @@
+/* 
+
+== Equal to value only
+=== Equal to value and Equalto type
+> greater than
+< less than
+>= greater than equal
+<= less than equal
+ ! NOT 
+ != EQUAL VALUE ONLY
+ !== NOT EQ VALUE AND TYPE
+
+ LOGICAL OPERATORS
+
+  && AND
+  || OR
+  ! NOT
+
+  THIS OPERATORS PROVIDE BOLEAN TYPE 
+  "TRUE OR FALSE"
+
+
+*/
+let result = (3==3 || 33==23);
+
+
+console.log(result);
